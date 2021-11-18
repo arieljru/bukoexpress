29693444ce1a2e2b7439366b0e87d11902fb89d2
arieljru <94475937+arieljru@users.noreply.github.com>
2021-11-18 11:52:31 -1100

    Update pred.py

--- pred.py
+++ pred.py
@@ -24,36 +24,7 @@ if st.button('Back'):
     div = Div(text=html)
     st.bokeh_chart(div)
 #ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-from multiprocessing import Process
-import streamlit as st
-import SessionState
-import time
-import os
-import signal
 
-st.sidebar.title("Controls")
-start = st.sidebar.button("Start")
-stop = st.sidebar.button("Stop")
-
-state = SessionState.get(pid=None)
-
-
-def job():
-    for _ in range(100):
-        print("In progress")
-        time.sleep(1)
-
-
-if start:
-    p = Process(target=job)
-    p.start()
-    state.pid = p.pid
-    st.write("Started process with pid:", state.pid)
-
-if stop:
-    os.kill(state.pid, signal.SIGKILL)
-    st.write("Stopped process with pid:", state.pid)
-    state.pid = None
 #ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 st.title("Predictive Analytics")
 
