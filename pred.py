2db5843f9f94e6fb7011e0fd0288b92cfffaad34
arieljru <94475937+arieljru@users.noreply.github.com>
2021-11-18 10:17:11 -1100

    Update pred.py

--- pred.py
+++ pred.py
@@ -10,15 +10,18 @@ import pandas as pd
 import numpy as np
 
 
+
+
+#Page expands to full width
+st.beta_set_page_config(layout="wide")
+
+
 st.markdown('''
     <a href="http://testingbig.rf.gd/big1/main/salesreport.php?d1=0&d2=0">
         Back
     </a>''',
     unsafe_allow_html=True
 )
-
-#Page expands to full width
-st.beta_set_page_config(layout="wide")
 
 st.title("Predictive Analytics")
 
