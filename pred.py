da7b70d4b8c78931d8a6471fda91dace3f15bf4b
arieljru <94475937+arieljru@users.noreply.github.com>
2021-11-19 02:51:59 -1100

    Update pred.py

--- pred.py
+++ pred.py
@@ -25,6 +25,12 @@ if st.button('Back'):
     st.bokeh_chart(div)
 
 #ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+if st.button('Go to Streamlit'):
+    js = "window.open('https://www.streamlit.io/')"  # New tab or window
+    js = "window.location.href = 'https://www.streamlit.io/'"  # Current tab
+    html = '<img src onerror="{}">'.format(js)
+    div = Div(text=html)
+    st.bokeh_chart(div)
 
 #ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 st.title("Predictive Analytics")
